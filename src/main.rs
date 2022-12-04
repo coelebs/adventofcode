dd0f04ee3f10aaae20e78aead08756f04da4461a
Vincent Kriek <vincent@kriek.org>
2022-12-04 20:56:45 +0100

    Be able to do multiple days.
    match is confusing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod day1;
 
 fn main() {
-    day1::part1();
-    day1::part2();
+    if let Some(arg1) = std::env::args().nth(1) {
+        if arg1 == "day1" {
+            day1::part1();
+            day1::part2();
+        }
+    }
 }
