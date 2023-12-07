d3c073f1ca6c427744e8a6c7efbe32678515b6e2
Vincent Kriek <vincent@kriek.org>
2023-12-07 20:15:51 +0100

    D5P2

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -56,38 +56,101 @@ fn part1(seeds: &Vec<i64>, mut sections: VecDeque<&str>) {
     info!("Lowest location seed: {result1}");
 }
 
-fn part2(_seeds: &Vec<i64>, mut sections: VecDeque<&str>) {
-    //create ranges
-    let mut ranges: Vec<Vec<Vec<i64>>> = vec![];
-    ranges.resize(sections.len(), vec![vec![0]]);
+fn process_section_range(section: &str, seedmap: &Vec<(i64, i64)>) -> Vec<(i64, i64)> {
+    debug!("Mapping {}", section.lines().next().unwrap());
+    let mut next = vec![];
+    let bools = vec![false; seedmap.len()];
+    let mut prev: Vec<((i64, i64), bool)> =
+        seedmap.iter().zip(bools).map(|x| (*x.0, x.1)).collect();
 
-    while sections.len() > 0 {
-        let section = sections.pop_back().unwrap();
+    for line in section.lines().skip(1) {
+        let map: Vec<i64> = line
+            .split(" ")
+            .filter(|x| !x.trim().is_empty())
+            .map(|x| x.parse::<i64>().unwrap_or(-1))
+            .collect();
 
-        section.lines().skip(1).for_each(|x| {
-            ranges[sections.len()].push(x.split(" ").map(|x| x.parse::<i64>().unwrap()).collect())
-        });
+        let mut i = 0;
+        while i < prev.len() {
+            if !prev[i].1 {
+                let start = prev[i].0 .0;
+                let end = prev[i].0 .0 + prev[i].0 .1;
+
+                let secstart = map[1];
+                let secend = map[1] + map[2];
+
+                if (start > secstart && start < secend) || (end > secstart && end < secend) {
+                    prev[i].1 = true;
+                    debug!("Overlap between: {start}:{end} and {secstart}:{secend}");
+                    if start >= secstart && end <= secend {
+                        debug!("Full overlap even");
+                        next.push((start - secstart + map[0], prev[i].0 .1));
+                    } else if start > secstart && start < secend {
+                        debug!("Partial overlap right");
+                        let matchend = secend - start;
+                        next.push(((start - secstart) + map[0], matchend));
+                        prev.push(((start + matchend, end - secend), false));
+                        debug!("Rest is {},{}", start + matchend, end - secend);
+                        debug!("Processed is {},{}", (start - secstart) + map[0], matchend);
+                    } else if end > secstart && end < secend {
+                        debug!("Partial overlap left");
+                        next.push((map[0], end - secstart));
+                        prev.push(((start, secstart - start), false));
+                        debug!("Rest is {},{}", start, secstart - start);
+                        debug!("Processed is {},{}", map[0], end - secstart);
+                    }
+                }
+            }
+            i += 1;
+        }
     }
 
-    debug!("{:?}", ranges);
+    for p in prev.iter().filter(|x| !x.1) {
+        debug!("{:?} stays the same", p.0);
+        next.push(p.0);
+    }
+
+    return next;
+}
+
+fn part2(seedranges: &mut Vec<(i64, i64)>, mut sections: VecDeque<&str>) {
+    let starttotalseeds: i64 = seedranges.iter().map(|x| x.1).sum();
+    let next = seedranges;
+    while sections.len() > 0 {
+        *next = process_section_range(sections.pop_front().unwrap(), &next);
+        let totalseeds: i64 = next.iter().map(|x| x.1).sum();
+        debug!("{totalseeds}/{starttotalseeds}");
+        debug!("next: {:#?}", next);
+    }
+
+    let totalseeds: i64 = next.iter().map(|x| x.1).sum();
+    debug!("{totalseeds}/{starttotalseeds}");
+
+    let result2 = next.iter().map(|x| x.0).min().unwrap();
+    info!("Ranges result: {result2}");
 }
 
 fn day5(input: String) {
     let mut sections = input.split("\n\n").collect::<VecDeque<&str>>();
 
-    let seeds: Vec<i64> = sections
-        .pop_front()
-        .unwrap()
-        .split_once(":")
-        .unwrap()
-        .1
+    let seed_section = sections.pop_front().unwrap().split_once(":").unwrap().1;
+    let seeds: Vec<i64> = seed_section
         .split(" ")
         .filter(|x| !x.trim().is_empty())
         .map(|x| x.parse::<i64>().unwrap_or(-1))
         .collect();
 
+    let mut seedranges: Vec<(i64, i64)> = vec![];
     part1(&seeds, sections.clone());
-    part2(&seeds, sections);
+    let seed_in: Vec<&str> = seed_section.split_ascii_whitespace().collect();
+    let mut i = 0;
+    while i < seed_in.len() {
+        seedranges.push((seed_in[i].parse().unwrap(), seed_in[i + 1].parse().unwrap()));
+        i += 2;
+    }
+    debug!("{:#?}", seedranges);
+
+    part2(&mut seedranges, sections);
 }
 
 fn main() {
